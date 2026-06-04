3b8b17b277ebe0c59c732486385f3ed69bdd135a
BUBLYAASH <bublikcsgo228@gmail.com>
2026-06-04 13:50:45 +0300

    Set up important files to work with. Made a directory architecture. Linked docker-compose and added Gradle work file.

---
+++ src/main/java/org/bublapi/dent/BublAPIDentApplication.java
@@ -0,0 +1,11 @@
+package org.bublapi.dent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BublAPIDentApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(BublAPIDentApplication.class, args);
+  }
+}
