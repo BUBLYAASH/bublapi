d2c5f39f26b3e3849fd52bbc850b9ca0ce7c1aa0
BUBLYAASH <bublikcsgo228@gmail.com>
2026-06-16 20:51:25 +0300

    - Removed extra imports - Added tags and operation summaries to endpoints to make it more convenient to use

--- src/main/java/org/bublapi/dent/clinic/controller/ClinicController.java
+++ src/main/java/org/bublapi/dent/clinic/controller/ClinicController.java
@@ -1,18 +1,14 @@
 package org.bublapi.dent.clinic.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
-import org.bublapi.dent.clinic.dto.ClinicResponseDto;
-import org.bublapi.dent.clinic.dto.CreateClinicRequestDto;
-import org.bublapi.dent.clinic.dto.UpdateClinicRequestDto;
+import org.bublapi.dent.clinic.dto.*;
 import org.bublapi.dent.clinic.service.ClinicService;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Clinics")
 @RestController
 @RequestMapping("/api/clinics")
 public class ClinicController {
@@ -22,11 +18,13 @@ public class ClinicController {
     this.clinicService = clinicService;
   }
 
+  @Operation(summary = "Create a new clinic", description = "Add a new clinic to DB")
   @PostMapping
   public ClinicResponseDto create(@Valid @RequestBody CreateClinicRequestDto request){
     return clinicService.create(request);
   }
 
+  @Operation(summary = "Update a clinic", description = "Update only provided fields in clinic")
   @PatchMapping("/{id}")
   public ClinicResponseDto update(@PathVariable UUID id, @Valid @RequestBody UpdateClinicRequestDto request){
     return clinicService.update(id, request);
