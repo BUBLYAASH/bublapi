8ddeeda4077db257ddc21b878e409fd807d8d7db
BUBLYAASH <bublikcsgo228@gmail.com>
2026-06-19 01:50:51 +0300

    feat: add method signature

--- src/main/java/org/bublapi/dent/user/repository/UserRepository.java
+++ src/main/java/org/bublapi/dent/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package org.bublapi.dent.user.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 import org.bublapi.dent.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+  Optional<User> findByIdAndClinic_Id(UUID userId, UUID clinicId);
 }
