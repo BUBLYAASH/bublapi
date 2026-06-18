276ce6c48f9b08e9752b1e2908f56d6a4bc34a23
BUBLYAASH <bublikcsgo228@gmail.com>
2026-06-19 01:51:42 +0300

    fix: add @Valid for Request Body in Update endpoint

--- src/main/java/org/bublapi/dent/doctor/controller/DoctorController.java
+++ src/main/java/org/bublapi/dent/doctor/controller/DoctorController.java
@@ -8,12 +8,18 @@ import org.bublapi.dent.doctor.dto.CreateDoctorRequestDto;
 import org.bublapi.dent.doctor.dto.DoctorResponseDto;
 import org.bublapi.dent.doctor.dto.UpdateDoctorRequestDto;
 import org.bublapi.dent.doctor.service.DoctorService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Doctors")
 @RestController
 @RequestMapping("/api/clinics/{clinicId}/doctors")
 public class DoctorController {
+
   private final DoctorService doctorService;
 
   public DoctorController(DoctorService doctorService) {
@@ -22,13 +28,15 @@ public class DoctorController {
 
   @PostMapping
   @Operation(summary = "Add a new doctor", description = "Add a new doctor to provided clinic")
-  public DoctorResponseDto createDoctor(@PathVariable UUID clinicId, @Valid @RequestBody CreateDoctorRequestDto request) {
+  public DoctorResponseDto createDoctor(@PathVariable UUID clinicId,
+      @Valid @RequestBody CreateDoctorRequestDto request) {
     return doctorService.create(clinicId, request);
   }
 
   @PatchMapping("/{doctorId}")
   @Operation(summary = "Update doctor's profile", description = "Update provided fields in doctor's profile")
-  public DoctorResponseDto updateDoctor(@PathVariable UUID clinicId, @PathVariable UUID doctorId, @RequestBody UpdateDoctorRequestDto request) {
+  public DoctorResponseDto updateDoctor(@PathVariable UUID clinicId, @PathVariable UUID doctorId,
+      @Valid @RequestBody UpdateDoctorRequestDto request) {
     return doctorService.update(clinicId, doctorId, request);
   }
 }
