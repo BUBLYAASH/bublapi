c59d096fd301d7a5ae043284389d7a79197585ba
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-04 19:14:18 +0300

    fix: fix requestMatchers for /api/public style: remove TODO

--- src/main/java/org/bublapi/dent/auth/config/SecurityConfig.java
+++ src/main/java/org/bublapi/dent/auth/config/SecurityConfig.java
@@ -4,7 +4,6 @@ package org.bublapi.dent.auth.config;
 import org.bublapi.dent.auth.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,11 +35,10 @@ public class SecurityConfig {
           .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
           .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
                                              .permitAll()
-                                             .requestMatchers(HttpMethod.GET, "/api/public/**")
+                                             .requestMatchers("/api/public/**")
                                              .permitAll()
                                              .anyRequest()
                                              .authenticated())
-          //TODO: /api/clinics/** permit only for ADMIN and all public methods put into /api/public/** endpoint
           .formLogin(AbstractHttpConfigurer::disable)
           .httpBasic(AbstractHttpConfigurer::disable)
           .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
