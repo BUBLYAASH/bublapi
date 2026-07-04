49b93ce37859e48650d055dcd58c72036bd3c3f0
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-05 00:00:29 +0300

    fix: RoleName instead of String to findByName a role

--- src/main/java/org/bublapi/dent/role/repository/RoleRepository.java
+++ src/main/java/org/bublapi/dent/role/repository/RoleRepository.java
@@ -1,6 +1,7 @@
 package org.bublapi.dent.role.repository;
 
 import org.bublapi.dent.role.entity.Role;
+import org.bublapi.dent.role.entity.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,5 @@ import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
 
-   Optional<Role> findByName(String name);
+   Optional<Role> findByName(RoleName name);
 }
