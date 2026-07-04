59d13f5ba18186ce6e5750645480af80f0f0ef34
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-04 19:14:38 +0300

    style: new line

--- src/main/java/org/bublapi/dent/appointment/repository/AppointmentRepository.java
+++ src/main/java/org/bublapi/dent/appointment/repository/AppointmentRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
+
    Optional<Appointment> findByIdAndClinic_IdAndPatient_Id(UUID appointmentId, UUID clinicId, UUID patientId);
 }
