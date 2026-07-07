24bba2ec238f6ff3ddddaa5a2806141fa0fd778d
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-08 00:28:50 +0300

    feat: ignore few new mappings

--- src/main/java/org/bublapi/dent/appointment/mapper/AppointmentMapper.java
+++ src/main/java/org/bublapi/dent/appointment/mapper/AppointmentMapper.java
@@ -9,13 +9,14 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = AppointmentServiceMapper.class)
 public interface AppointmentMapper {
+   @Mapping(target = "id", ignore = true)
    @Mapping(target = "clinic", ignore = true)
    @Mapping(target = "patient", ignore = true)
    @Mapping(target = "doctor", ignore = true)
+   @Mapping(target = "services", ignore = true)
    @Mapping(target = "endAt", ignore = true)
    @Mapping(target = "totalPrice", ignore = true)
    @Mapping(target = "status", ignore = true)
-   @Mapping(target = "createdAt", ignore = true)
    Appointment toEntity(CreateAppointmentRequestDto request);
 
    @Mapping(target = "clinicId", source = "clinic.id")
