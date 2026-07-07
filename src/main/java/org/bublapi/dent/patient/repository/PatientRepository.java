1dece6540e42d170acef8d88b7674c39427b0fbb
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-08 00:32:10 +0300

    refactor: remove redundant method

--- src/main/java/org/bublapi/dent/patient/repository/PatientRepository.java
+++ src/main/java/org/bublapi/dent/patient/repository/PatientRepository.java
@@ -3,13 +3,10 @@ package org.bublapi.dent.patient.repository;
 import org.bublapi.dent.patient.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
-
-   List<Patient> findAllByClinic_Id(UUID id);
 
    Optional<Patient> findByPhone(String phone);
 
