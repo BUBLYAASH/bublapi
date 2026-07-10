b29a18697fcac512b36f1362201b3f5603d2ed6f
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-11 02:26:53 +0300

    feat: add login for admin

--- src/main/java/org/bublapi/dent/auth/service/AuthService.java
+++ src/main/java/org/bublapi/dent/auth/service/AuthService.java
@@ -6,7 +6,7 @@ import org.bublapi.dent.auth.dto.RegisterRequestDto;
 import org.bublapi.dent.common.context.ClinicContext;
 import org.bublapi.dent.common.exception.BadRequestException;
 import org.bublapi.dent.user.dto.CreateUserRequestDto;
-import org.bublapi.dent.user.dto.UserResponseDto;
+import org.bublapi.dent.user.dto.CreateUserResponseDto;
 import org.bublapi.dent.user.entity.User;
 import org.bublapi.dent.user.repository.UserRepository;
 import org.bublapi.dent.user.service.UserService;
@@ -50,7 +50,28 @@ public class AuthService {
       return new LoginResponseDto(token);
    }
 
-   public UserResponseDto register(RegisterRequestDto request) {
-      return userService.create(new CreateUserRequestDto(request.email(), request.phone(), request.firstName(), request.lastName(), request.middleName(), request.password()));
+   public LoginResponseDto loginAdmin(LoginRequestDto request) {
+      User user = userRepository.findAdminByEmailWithRoles(request.email())
+                                .orElseThrow(() -> new BadRequestException("Invalid email or password"));
+
+      boolean passwordMatches = passwordEncoder.matches(request.password(), user.getPasswordHash());
+
+      if (!passwordMatches) {
+         throw new BadRequestException("Invalid email or password");
+      }
+
+      if (!user.getEnabled()) {
+         throw new BadRequestException("User is disabled");
+      }
+
+      String token = jwtService.generateToken(user.getId());
+
+      return new LoginResponseDto(token);
+   }
+
+   public CreateUserResponseDto register(RegisterRequestDto request) {
+      return userService.create(
+              new CreateUserRequestDto(request.email(), request.phone(), request.firstName(), request.lastName(),
+                                       request.middleName(), request.password()));
    }
 }
