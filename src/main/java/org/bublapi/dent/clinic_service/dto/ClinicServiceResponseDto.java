ff8b3877e975d19b469e3004cce4d274921911a0
BUBLYAASH <bublikcsgo228@gmail.com>
2026-07-20 22:18:21 +0300

    feat: add active field in response DTO

--- src/main/java/org/bublapi/dent/clinic_service/dto/ClinicServiceResponseDto.java
+++ src/main/java/org/bublapi/dent/clinic_service/dto/ClinicServiceResponseDto.java
@@ -3,5 +3,6 @@ package org.bublapi.dent.clinic_service.dto;
 import java.util.UUID;
 
 public record ClinicServiceResponseDto(
-        UUID id, UUID clinicId, UUID dentalServiceId, String title, Integer price, Integer durationMinutes) {
+        UUID id, UUID clinicId, UUID dentalServiceId, String title, Integer price, Integer durationMinutes,
+        boolean active) {
 }
