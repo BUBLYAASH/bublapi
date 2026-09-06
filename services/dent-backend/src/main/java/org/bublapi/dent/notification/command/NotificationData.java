e243cfcd92c79649a0edf06c0cc19634cc624c80
BUBLYAASH <bublikcsgo228@gmail.com>
2026-09-06 15:26:54 +0300

    fix: fix json rabbit info

--- services/dent-backend/src/main/java/org/bublapi/dent/notification/command/NotificationData.java
+++ services/dent-backend/src/main/java/org/bublapi/dent/notification/command/NotificationData.java
@@ -1,4 +1,12 @@
 package org.bublapi.dent.notification.command;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "dataType")
+@JsonSubTypes({@JsonSubTypes.Type(value = AppointmentNotificationData.class, name = "appointment"),
+               @JsonSubTypes.Type(value = UserNotificationData.class, name = "user"),
+               @JsonSubTypes.Type(value = PatientCardNotificationData.class, name = "patientCard"),
+               @JsonSubTypes.Type(value = ClinicServiceNotificationData.class, name = "clinicService")})
 public sealed interface NotificationData permits AppointmentNotificationData, UserNotificationData, PatientCardNotificationData, ClinicServiceNotificationData {
 }
